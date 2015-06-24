c5a5a3095138cd01693f712360ca75661a076bac
dea-yong <zghdeodydz@naver.com>
2015-06-24 17:15:50 +0900

    add ch06

---
+++ ch06/2.6/call/dev/.call_dev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/dy/device/ch06/2.6/call/dev/call_dev.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/dy/device/ch06/2.6/call/dev/call_dev.ko /home/dy/device/ch06/2.6/call/dev/call_dev.o /home/dy/device/ch06/2.6/call/dev/call_dev.mod.o
---
+++ ch06/2.6/call/dev/.tmp_versions/call_dev.mod
@@ -0,0 +1,2 @@
+/home/dy/device/ch06/2.6/call/dev/call_dev.ko
+/home/dy/device/ch06/2.6/call/dev/call_dev.o
